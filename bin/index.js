1cf250035965e95f509b807ff1ee76ef482d648b
dykoffi <dykoffi2000@gmail.com>
2022-11-21 05:23:10 +0000

    Reuse cmd

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,65 @@
-import init from "./init";
+#!/usr/bin/env node
 
-init("test-create", "for world initialization")
+/*This file is the input file of the cqx module. it must not be modified*/
+
+// Import Library
+
+import { Command } from "commander";
+
+// Import Commands
+
+import init from "./commands/init.js";
+
+// Import Commands
+
+import {
+    logError,
+} from "../libs/utils.js";
+
+// Define variables
+
+const program = new Command();
+
+// Define commands
+
+program
+    .command("<folder>")
+    .description("Initiating a cqx project")
+    .option("-d, --description <description>", "Description of the project")
+    .option("--ci", "Add .circleci/config.yml file")
+    .option(
+        "--setup",
+        "Generate setup.sh file to setup project on github and circleci"
+    )
+    .option("--gregion <gregion>", "Google compute regio,", "europe-north1")
+    .option("--gproject <gproject>", "Google project ID", "cqx")
+    .option(
+        "-m, --modules-path <modules_path>",
+        "Copy an existing node_modules and yarn.lock in the project"
+    )
+    .action((folder, options) => {
+        console.time("time");
+        init(
+                folder,
+                options.description,
+                options.ci,
+                options.setup,
+                options.gregion,
+                options.gproject,
+                options.modulesPath
+            )
+            .then(() => {
+                console.timeEnd("time");
+            })
+            .catch((err) => {
+                logError(err);
+                process.exit(1);
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    process.exit(0);
+                }, 500);
+            });
+    });
+
+program.parse(process.args);
